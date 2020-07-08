3b449799c1b7982c7d51ea6f8ad6c834ac41d91b
alexmedero10 <43555049+alexmedero10@users.noreply.github.com>
2020-07-07 22:37:32 -0500

    Guarda el clique

---
+++ reporter.py
@@ -0,0 +1,20 @@
+class Reporter(object):
+    def __init__(self, name):
+        self.name = name
+        self.cnt = 0
+        self.cliques = []
+        self.tam = 0
+ 
+    def inc_count(self):
+        self.cnt += 1
+ 
+    def record(self, clique):
+        if(len(clique) > self.tam):
+            self.cliques = clique
+            self.tam = len(clique)
+ 
+    def print_report(self):
+        print(self.name)
+        print('%d recursive calls' % self.cnt)
+        print(self.cliques)
+        print()
