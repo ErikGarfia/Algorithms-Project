4c694816a76719cc60d987058880145b156e443e
alexmedero10 <43555049+alexmedero10@users.noreply.github.com>
2020-07-07 22:29:16 -0500

    Delete maximal_clique.py

--- maximal_clique.py
+++
@@ -1,9 +0,0 @@
-from bron_kerbosch import IK
-from data import *
-from reporter import Reporter
- 
- 
-if __name__ == '__main__':
-	report = Reporter('## %s' % IK.__doc__)
-	IK([], set(NODES), set(), report) 
-	report.print_report()
